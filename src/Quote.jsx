bf97ed9bee0d03ba0ce8696d4fef3ee7f300d1cc
Alex <alexgithinji44@gmail.com>
2024-06-06 14:45:14 +0300

    added a loading effect

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,25 +1,48 @@
 import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
+
+// const override = {
+//     display: "block",
+//     marigin: "0 auto"
+//     border-color: "blue",
+// }
+
+const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "purple",
+  };
 
 const Quote = () => {
     const [quote, setQuote] = useState("");
+    const [loading, setLoading] = useState(false);
+    let [color, setColor] = useState("")
+
 
    const displayQuote = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
      const response  = await  fetch ("https://api.adviceslip.com/advice")
      const data = await response.json();
      setQuote(data.slip.advice);
+     setLoading(false);
    }
 
     return(
         <div className="container">
             <h1>Get a new Quote anytime</h1>
-            <p>Advice can  be generated anytime you want , click the button below to get one</p>
+            <p>Quote can  be generated anytime you want , click the button below to get one</p>
 
             <button onClick={displayQuote}>click me</button>
+            {loading ? (
+                <ClipLoader color={color} loading={loading} cssOverride={override} s ize={45} />
+            ) : (
 
             <h2 >{quote}</h2>
-
+        )}
     
 
         </div>
